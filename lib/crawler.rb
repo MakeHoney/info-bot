949eab6b1260b23e092e54d434df36088f71180f
MakeHoney <makehoney3@gmail.com>
2018-04-11 21:07:06 +0900

    패스트푸드점 구획 나누기위해서 partition 함수 수정

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -19,7 +19,9 @@ require 'nokogiri'
 		end
 		
 		def partition(string)
-			if string.include?("<") || string.include?(">") || string.include?("운영")
+			if (string.include?("<") || string.include?(">") || 
+				(string.include?("운영") && string.include?("시간")) || 
+				string.include?("Burger")) # && !string.include?("택") 을 넣을까..?
 				return true
 			else
 				return false
